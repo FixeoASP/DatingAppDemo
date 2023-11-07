d715f59303749cbe1b8e1bde4b2877cea92cacde
giang phi <giang1510@hotmail.de>
2023-11-07 17:23:19 +0100

    Persisting login by using local storage (storing temp user data)

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,15 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { IUser } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AccountService {
+  static readonly USER_LOCAL_STORAGE_KEY = 'user';
   baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new BehaviorSubject<IUser | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
   login(model: any){
-    return this.http.post(this.baseUrl + 'account/login', model);
+    return this.http.post<IUser>(this.baseUrl + 'account/login', model).pipe(
+      map((response: IUser) => {
+        const user = response;
+        localStorage.setItem(AccountService.USER_LOCAL_STORAGE_KEY, JSON.stringify(user));
+        this.currentUserSource.next(user);
+      })
+    );
+  }
+
+  setCurrentUser(user: IUser){
+    this.currentUserSource.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem(AccountService.USER_LOCAL_STORAGE_KEY);
+    this.currentUserSource.next(null);
   }
 }
