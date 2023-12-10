ea0126bfc295eb586f670b3a5985d103283486e3
giang phi <giang1510@hotmail.de>
2023-12-10 23:07:09 +0100

    Handling errors the simple way (not recommended - better at the top level)

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -34,9 +34,16 @@ public class BuggyController : BaseApiController
     [HttpGet("server-error")]
     public ActionResult<string> GetServerError()
     {
-        var thing = _context.Users.Find(-1);
-        var thingToReturn = thing.ToString();
-        return thingToReturn;
+        try
+        {
+            var thing = _context.Users.Find(-1);
+            var thingToReturn = thing.ToString();
+            return thingToReturn;
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Computer says no!");
+        }
     }
 
     [HttpGet("bad-request")]
