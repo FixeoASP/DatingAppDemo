92d5c33b735f9441251d36fc33b81ae8bc5a087b
giang phi <giang1510@hotmail.de>
2023-12-12 21:03:40 +0100

    Handling not found error

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 // Path constants
 export const APP_ROUTES = {
@@ -30,7 +32,9 @@ const routes: Routes = [
     ]
   },
   {path: 'errors', component: TestErrorComponent},
-  {path: '**', component: HomeComponent, pathMatch: 'full'}
+  {path: 'not-found', component: NotFoundComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: '**', component: NotFoundComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
