c42ad0b7b1683724adc63336050d2f749e81bd45
giang phi <giang1510@hotmail.de>
2023-12-24 14:35:12 +0100

    Add photo gallery and make member detail component a standalone one

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,16 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 import { APP_ROUTES } from 'src/app/app-routing.module';
 
 @Component({
   selector: 'app-member-detail',
+  standalone: true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  imports: [CommonModule, TabsModule, GalleryModule]
 })
 export class MemberDetailComponent {
   member: Member | undefined;
+  images: GalleryItem[] = [];
 
   constructor(private memberService: MembersService, private route: ActivatedRoute){}
 
@@ -22,7 +28,20 @@ export class MemberDetailComponent {
     const username = this.route.snapshot.paramMap.get(APP_ROUTES.ROUTE_MEMBER_DETAIL_PARAM_USERNAME);
     if(!username) return;
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {
+        this.member = member;
+        this.getImages();
+      }
     });
+  }
+
+  getImages(){
+    if(!this.member) return;
+    for(const photo of this.member.photos){
+      this.images.push(new ImageItem({
+        src: photo.url,
+        thumb: photo.url
+      }));
+    }
   }
 }
