cbb178e8105e7528d3a370d7c5013bcb93c1d291
giang phi <giang1510@hotmail.de>
2024-01-12 08:05:40 +0100

    Add fallback controller to give back angular the routing responsibility when no endpoint is found

--- API/Program.cs
+++ API/Program.cs
@@ -27,6 +27,7 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapControllers();
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
