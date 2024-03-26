1cbd9cadcc7a0af7cd3140b90d1b304a7c5e4e80
giang phi <giang1510@hotmail.de>
2024-03-26 19:08:37 +0100

    API|Implement simple pagination

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using API.Helpers;
+
+namespace API.Extensions;
+
+public static class HttpExtensions
+{
+    private const string PaginationHeaderKey = "Pagination";
+
+    public static void AddPaginationHeader(this HttpResponse response, PaginationHeader header)
+    {
+        var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        response.Headers.Add(PaginationHeaderKey, JsonSerializer.Serialize(header, jsonOptions));
+        response.Headers.Add(
+            Microsoft.Net.Http.Headers.HeaderNames.AccessControlExposeHeaders,
+            PaginationHeaderKey);
+    }
+}
