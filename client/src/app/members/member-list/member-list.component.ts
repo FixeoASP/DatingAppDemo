7be82d5a3949cd892508607755bd7bbc4b1ae26a
giang phi <giang1510@hotmail.de>
2024-03-27 15:31:26 +0100

    Implement pagination on client side - show list items according to parameters

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
+import { PaginatedResult } from 'src/app/_models/paginatedResult';
+import { Pagination } from 'src/app/_models/pagination';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -9,12 +11,27 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent {
-  members$: Observable<Member[]> | undefined; 
+  // members$: Observable<PaginatedResult<Member[]>> | undefined; 
+  members: Member[] = [];
+  pagination: Pagination | undefined;
+  pageNumber = 1;
+  pageSize = 5;
 
   constructor(private memberService: MembersService){}
 
   ngOnInit(): void{
-    this.members$ = this.memberService.getMembers();
+    // this.members$ = this.memberService.getMembers();
+    this.loadMember();
   }
 
+  loadMember(){
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: response => {
+        if(response.result && response.pagination){
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+      }
+    })
+  }
 }
