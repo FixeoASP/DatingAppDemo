18e59eb06c3e96ddec564cf7e401fd34c3906fd7
giang phi <giang1510@hotmail.de>
2024-04-02 22:01:37 +0200

    client|Implement filtering with age and gender

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -20,6 +20,10 @@ export class MemberListComponent {
   pagination: Pagination | undefined;
   userParams : UserParams | undefined;
   user: User | undefined;
+  genderList = [
+    {value: 'male', display: 'Males'},
+    {value: 'female', display: 'Females'}
+  ];
 
   constructor(private memberService: MembersService, private accountService: AccountService){
     this.accountService.currentUser$.pipe(take(1)).subscribe({
@@ -52,6 +56,13 @@ export class MemberListComponent {
   pageChanged(event: PageChangedEvent){
     if(this.userParams && this.userParams.pageNumber  !== event.page){
       this.userParams.pageNumber = event.page;
+      this.loadMember();
+    }
+  }
+
+  resetFilters(){
+    if(this.user){
+      this.userParams = new UserParams(this.user);
       this.loadMember();
     }
   }
