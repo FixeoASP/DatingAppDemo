974403cec556a067dfa7c6b24d57fd893aa8e088
giang phi <giang1510@hotmail.de>
2024-04-16 16:07:26 +0200

    client|Remembering member filter

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -19,21 +19,13 @@ export class MemberListComponent {
   members: Member[] = [];
   pagination: Pagination | undefined;
   userParams : UserParams | undefined;
-  user: User | undefined;
   genderList = [
     {value: 'male', display: 'Males'},
     {value: 'female', display: 'Females'}
   ];
 
-  constructor(private memberService: MembersService, private accountService: AccountService){
-    this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: user => {
-        if(user){
-          this.userParams = new UserParams(user);
-          this.user = user;
-        }
-      }
-    })
+  constructor(private memberService: MembersService){
+    this.userParams = this.memberService.getUserParams();
   }
 
   ngOnInit(): void{
@@ -43,6 +35,7 @@ export class MemberListComponent {
 
   loadMember(){
     if(!this.userParams) return;
+    this.memberService.setUserParams(this.userParams);
     this.memberService.getMembers(this.userParams).subscribe({
       next: response => {
         if(response.result && response.pagination){
@@ -56,14 +49,13 @@ export class MemberListComponent {
   pageChanged(event: PageChangedEvent){
     if(this.userParams && this.userParams.pageNumber  !== event.page){
       this.userParams.pageNumber = event.page;
+      this.memberService.setUserParams(this.userParams);
       this.loadMember();
     }
   }
 
   resetFilters(){
-    if(this.user){
-      this.userParams = new UserParams(this.user);
-      this.loadMember();
-    }
+    this.userParams = this.memberService.resetUserParams();
+    this.loadMember();
   }
 }
