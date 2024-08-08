a27d1f384af5b16c14b91be5b88a5ef39a3c7fda
giang phi <giang1510@hotmail.de>
2024-08-08 19:42:33 +0200

    API|Identity usage - Database migration

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -20,16 +20,16 @@ public class DataContext : IdentityDbContext<AppUser, AppRole, int,
     {
         base.OnModelCreating(builder);
 
+        builder.Entity<AppUser>()
+            .HasMany(ur => ur.UserRoles)
+            .WithOne(u => u.User)
+            .HasForeignKey(ur => ur.UserId)
+            .IsRequired();
+
         builder.Entity<AppRole>()
             .HasMany(ur => ur.UserRoles)
             .WithOne(u => u.Role)
             .HasForeignKey(ur => ur.RoleId)
-            .IsRequired();
-
-        builder.Entity<AppUser>()
-            .HasMany(ur => ur.UserRoles)
-            .WithOne(u => u.User)
-            .HasForeignKey(u => u.User.Id)
             .IsRequired();
 
         builder.Entity<UserLike>()
