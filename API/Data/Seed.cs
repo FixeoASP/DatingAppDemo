8017cba7997d60ccc4f21a832821d14e198b5aa6
giang phi <giang1510@hotmail.de>
2024-08-12 14:48:01 +0200

    API|Update seed method

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -2,15 +2,16 @@
 using System.Text;
 using System.Text.Json;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(DataContext context)
+    public static async Task SeedUsers(UserManager<AppUser> userManager)
     {
-        if (await context.Users.AnyAsync()) return;
+        if (await userManager.Users.AnyAsync()) return;
 
         var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
 
@@ -20,9 +21,8 @@ public class Seed
         foreach (var user in users)
         {
             user.UserName = user.UserName.ToLower();
-            context.Users.Add(user);
-        }
 
-        await context.SaveChangesAsync();
+            await userManager.CreateAsync(user, "Pa$$w0rd");
+        }
     }
 }
